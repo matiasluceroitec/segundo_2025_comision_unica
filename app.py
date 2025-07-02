f1fa48980288f38a36646c69b2be6e6faa90d148
matias <matiasjavierlucero@gmail.com>
2025-07-01 22:48:12 -0300

    lees login

--- app.py
+++ app.py
@@ -4,27 +4,64 @@ from datetime import datetime
 from flask import Flask, flash, render_template, request, redirect, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import (
+    LoginManager,
+    login_user,
+    login_required,
+    logout_user,
+    current_user,
+)
+from werkzeug.security import (
+    generate_password_hash,
+    check_password_hash
+)
 
 app = Flask(__name__)
 
 app.secret_key = "cualquiercosa"
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    "mysql+pymysql://root:@localhost/segundo_unificado"
+    "mysql+pymysql://root:@localhost/segundo-com-a"
 )
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-from models import City, Climate
+from models import City, Climate, User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
     return render_template(
-        'index.html'
+        'index.html',
     )
 
+@app.route('/login')
+def login():
+    return render_template(
+        'auth/login.html'
+    )
+
+@app.route('/register')
+def register():
+    return render_template(
+        'auth/register.html'
+    )
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
 @app.route('/city', methods=['POST', 'GET'])
+@login_required
 def city():
     if request.method == "POST":
         name = request.form["name"]
