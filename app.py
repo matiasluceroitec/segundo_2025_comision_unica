64ed8a031e4ca041d759c391030e809ee0aa829b
matias <matiasjavierlucero@gmail.com>
2025-07-02 21:23:13 -0300

    clase preefi- flask login

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ from datetime import datetime
 from flask import Flask, flash, render_template, request, redirect, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import (
+    LoginManager,
+    login_user,
+    login_required,
+    logout_user,
+    current_user,
+)
+from werkzeug.security import (
+    check_password_hash,
+    generate_password_hash
+    )
+
 
 app = Flask(__name__)
 
@@ -15,8 +27,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-from models import City, Climate
+
+from models import City, Climate, User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
@@ -24,7 +44,68 @@ def index():
         'index.html'
     )
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password'] # Pass que llega desde el formulario
+
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(pwhash=user.password_hash, password=password):
+            login_user(user)
+            return redirect(url_for('index'))
+
+    return render_template(
+        'auth/login.html'
+    )
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    if request.method == 'POST':
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username exist', 'error')
+            return redirect(url_for('register'))
+        
+        # Hasheo de contraseña
+        password_hash = generate_password_hash(
+            password=password,
+            method='pbkdf2'
+        )
+        # Creacion del nuevo usuario
+        new_user = User(
+            username=username,
+            email=email,
+            password_hash=password_hash
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Username created succefully', 'success')
+        return redirect(url_for('login'))
+
+
+    return render_template(
+        'auth/register.html'
+    )
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route('/city', methods=['POST', 'GET'])
+#@login_required
 def city():
     if request.method == "POST":
         name = request.form["name"]
@@ -47,9 +128,15 @@ def city():
 @app.route('/city/<int:city_id>')
 def city_detail(city_id):
     city = City.query.get_or_404(city_id)
+    weathers = Climate.query.filter_by(
+        ciudad_id=city.id
+        ).filter_by(
+            user_id=current_user.id
+        ).all()
     return render_template(
         'city_detail.html', 
         city=city, 
+        weathers=weathers
     )
 
 @app.route('/city/<int:city_id>/get_current_climate')
@@ -63,6 +150,7 @@ def get_current_climate(city_id):
         temperature=data['current_weather']['temperature'],
         windspeed=data['current_weather']['windspeed'],
         winddirection=data['current_weather']['winddirection'],
+        user_id=current_user
     )
     db.session.add(clima_actual)
     db.session.commit()
--- models.py
+++ models.py
@@ -1,5 +1,23 @@
 from app import db
 
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(256), nullable=False)
+    is_active = db.Column(db.Boolean, default=True) 
+    climates = db.relationship(
+        'Climate',
+        backref='user',
+        lazy=True
+    )
+
+
+    def __str__(self):
+        return self.username
 
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,6 +41,7 @@ class Climate(db.Model):
     ciudad_id = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
     windspeed = db.Column(db.Float, nullable=True)
     winddirection = db.Column(db.Float, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __str__(self) -> str:
         return f"{self.date} {self.ciudad_id} {self.temperature}"
